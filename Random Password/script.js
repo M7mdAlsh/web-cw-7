a60ce676dcb4fb08429c6afbb19239d418ed2f34
M7mdAlsh <100633304+M7mdAlsh@users.noreply.github.com>
2022-03-26 08:43:18 +0300

    The_End

--- Random Password/script.js
+++ Random Password/script.js
@@ -1,3 +1,39 @@
 // Database قاعدة البيانات
+const chars = [{1:`1234567890`}, 
+{2:`QWASZXDCEVFRGTBHNYJUMKILPO`},
+ {3:`qwaszedxrctfgvbgnhmukjipmlo`}, 
+ {4:`!@#$%^&*()"?:><{}`}]
 
 // دالة generate
+function generate() {
+
+
+    let randompassword = "" 
+
+
+    let collection_1 = chars [0] ["c"]
+    let collection_2 = chars [1] ["2"]
+    let collection_3 = chars [2] ["3"]
+    let collection_4 = chars [3] ["4"]
+
+ randomPassword = 
+ randomPassword + 
+ collection_1.charAt(Math.floor(Math.random() * collection_1.length));
+
+ randomPassword = 
+ randomPassword + 
+ collection_2.charAt(Math.floor(Math.random() * collection_2.length));
+
+
+ randomPassword = 
+ randomPassword + 
+ collection_3.charAt(Math.floor(Math.random() * collection_3.length));
+
+
+ randomPassword = 
+ randomPassword + 
+ collection_4.charAt(Math.floor(Math.random() * collection_4.length));
+
+
+display.value = randompassword;
+}
--- Random/script.js
+++ Random/script.js
@@ -1,3 +1,22 @@
 // project أنشئ كائن
+const project = {
+    type: `man`,
+    users: 200,
+    biography() {    
+        return `test test test`
+    }
+    
+}
 
-// randomProperty أنشئ دالة
+
+    // randomProperty أنشئ دالة
+
+    function randomProperty(o) {
+        const keys = object.keys(o);
+    
+        const random = Math.floor(Math.random() * keys.length)
+    
+    }
+    
+    randomProperty(project)
+   
